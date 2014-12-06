b2666b83ca2b57fa43872e5b95866c600e503251
milaq <micha.laqua@gmail.com>
2014-12-06 16:51:02 +0100

    update exchange policy patch for LP

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -34,3 +34,20 @@ function check_clean {
 # insert cherry-picks below
 #
 
+# Email: Add support for ignoring exchange server policy (1/2)
+PATCH=14-12-06_Add-support-for-ignoring-exchange-server-policy-1-2
+FOLDER=packages/apps/Email
+###
+pushd ${FOLDER}
+wget https://raw.github.com/milaq/android/cm-12.0/patches/${PATCH}.patch
+git am ${PATCH}.patch
+check_clean
+
+# Exchange: Add support for ignoring exchange server policy (2/2)
+PATCH=14-12-06_Add-support-for-ignoring-exchange-server-policy-2-2
+FOLDER=packages/apps/Exchange
+###
+pushd ${FOLDER}
+wget https://raw.github.com/milaq/android/cm-12.0/patches/${PATCH}.patch
+git am ${PATCH}.patch
+check_clean
