219b44e1bd49036cf6dbad5448b08baf54390c08
milaq <micha.laqua@gmail.com>
2016-03-15 14:20:53 +0100

    refresh gb branch

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -1,45 +1,58 @@
 #!/bin/bash
-#
+# ---------------------------------------------------------
 # cherry-picks
-# remember to cd into and out of the directories
+# use pushd to enter directories
 #
-# add like this:
+# add cherry-picks like this:
 #
-# mkbootimg: support pagesize 8192
-#cd system/core
-#git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
-#cd ../..
-#
+# # mkbootimg: support pagesize 8192
+# pushd system/core
+# git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
+# check_clean
+# ---------------------------------------------------------
 
+function check_clean {
+  if [ -e *.patch ]
+  then
+    rm *.patch
+  fi
+  if [ -e ".git/rebase-apply" ]
+  then
+    git am --abort
+    popd
+    exit 1
+  elif [ -e ".git/CHERRY_PICK_HEAD" ]
+  then
+    git cherry-pick --abort
+    popd
+    exit 1
+  fi
+  popd
+}
+
+function apply {
+  pushd $2
+  wget https://raw.githubusercontent.com/milaq/android/$BRANCH/patches/$1.patch
+  git am $1.patch
+  check_clean
+}
+
+#
+# insert cherry-picks below
+#
+BRANCH=gingerbread
 
 # add lenovo a107 to targets
-cd vendor/cyanogen
-wget https://raw.github.com/milaq/android/gingerbread/patches/23-03-13_add-lenovo-a107-IdeaPad-A1.patch
-git am 23-03-13_add-lenovo-a107-IdeaPad-A1.patch
-rm 23-03-13_add-lenovo-a107-IdeaPad-A1.patch
-cd ../..
+apply add-lenovo-a107-IdeaPad-A1 vendor/cyanogen
 
 # add htc kovsky to targets
-cd vendor/cyanogen
-wget https://raw.github.com/milaq/android/gingerbread/patches/24-03-13_add-htc-kovsky.patch
-git am 24-03-13_add-htc-kovsky.patch
-rm 24-03-13_add-htc-kovsky.patch
-cd ../..
+apply add-htc-kovsky vendor/cyanogen
 
 # Add option to configure ramdisks built with mkimage for U-Boot (needed for a107)
-cd build
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/20/11920/1 && git cherry-pick FETCH_HEAD
-cd ..
+apply custom-mkimage-ramdisks build
 
 # SensorService: Don't always assume magnetometer is present (needed for a107)
-cd frameworks/base
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/56/16156/1 && git cherry-pick FETCH_HEAD
-cd ../..
+apply no-magnetometer frameworks/base
 
 # Added driver_cmd function to nl80211 driver (needed for kovsky)
-cd external/wpa_supplicant_6
-wget https://raw.github.com/milaq/android/gingerbread/patches/24-03-13_nl80211_driver_cmd.patch
-git am 24-03-13_nl80211_driver_cmd.patch
-rm 24-03-13_nl80211_driver_cmd.patch
-cd ../..
-
+apply nl80211_driver_cmd external/wpa_supplicant_6
